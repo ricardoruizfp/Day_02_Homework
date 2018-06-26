9d691e1b42c3d9461178167e3f42e3b4445938c0
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-06-26 16:53:13 +0100

    num to full month 1

--- ruby_functions.rb
+++ ruby_functions.rb
@@ -4,3 +4,43 @@
 def return_10()#dont need to add brackets as it does not reciev arguments
   return 20/2
 end
+
+def add(first, second)
+  return (first + second)
+end
+
+def subtract(first, second)
+  return (first - second)
+end
+
+def multiply (first, second)
+  return (first * second)
+end
+
+def divide (first, second)
+  return (first / second)
+end
+
+def length_of_string (test_string)
+  length = test_string.length
+  return length
+end
+
+def join_string(string_1, string_2)
+  return "#{string_1}#{string_2}"
+end
+
+def add_string_as_number(x, y)
+  x_as_integer = x.to_i
+  y_as_integer = y.to_i
+  sum = x_as_integer + y_as_integer
+  return sum
+end
+
+def number_to_full_month_name(month_number)
+  case month_number
+  when 1
+    return "January"
+
+  end
+end
