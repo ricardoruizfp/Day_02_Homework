66298c65aca48dca20d1cd4d9e04739a4c174e27
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-06-26 15:55:20 +0100

    solved multiply test

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -17,11 +17,11 @@ def test_add()
     subtract_result = subtract( 10, 5 )
     assert_equal( 5, subtract_result )
   end
-#
-#   def test_multiply()
-#     multiply_result = multiply( 4, 2 )
-#     assert_equal( 8, multiply_result )
-#   end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
 #
 #   def test_divide()
 #     divide_result = divide( 10, 2 )
